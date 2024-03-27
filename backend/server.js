e71b7e274cfe912a3beaff184a8f5d8cade0fc23
ncoder <bhargavayash2015@gmail.com>
2024-03-28 00:26:08 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,25 @@
 const express = require("express");
-const app = express();
 require("dotenv").config();
 var cors = require('cors');
+const app = express();
 app.use(cors());
 const { dbConnect } = require('./config/dbConnect');
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
 const { cloudinaryConnect } = require('./config/cloudinaryConnect');
 const fileUpload = require('express-fileupload');
+const path = require("path");
+const __dirname = path.resolve();
 cloudinaryConnect();
 dbConnect();
 app.use(fileUpload());
 app.use(express.json());
-app.get('/', (req,res) => {
-    return res.json({
-        data: "Server is runing"
-    })
-})
 app.use('/api/user', userRoutes);
 app.use('/api/post', postRoutes);
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'))
+});
 app.listen(process.env.PORT, () => {
     console.log(`
 Server is running at ${process.env.PORT}`.underline.cyan);
