87e81e93916351813abb48e0d5091f86d992eafa
ncoder <bhargavayash2015@gmail.com>
2024-03-28 00:32:58 +0530

    removed dirname

--- backend/server.js
+++ backend/server.js
@@ -9,16 +9,16 @@ const postRoutes = require('./routes/postRoutes');
 const { cloudinaryConnect } = require('./config/cloudinaryConnect');
 const fileUpload = require('express-fileupload');
 const path = require("path");
-const __dirname = path.resolve();
+
 cloudinaryConnect();
 dbConnect();
 app.use(fileUpload());
 app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use('/api/post', postRoutes);
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(path.resolve(), '../client/build')));
 app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'))
+    res.sendFile(path.join(path.resolve(), '../client', 'build', 'index.html'))
 });
 app.listen(process.env.PORT, () => {
     console.log(`
